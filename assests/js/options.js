dca7cc67a0db8c67ff1f9bb624571d610613808e
RobertJGabriel <robert_gabriel@outlook.com>
2015-02-10 22:07:56 +0000

    White space
    White spaces

--- assests/js/options.js
+++ assests/js/options.js
@@ -1,9 +1,7 @@
 // Saves options to chrome.storage
 function save_options() {
-
     var likesColor = document.getElementById('like').checked;
     chrome.storage.sync.set({
-
         likesColor: likesColor
     }, function() {
         // Update status to let user know options were saved.
@@ -12,23 +10,21 @@ function save_options() {
         setTimeout(function() {
             status.textContent = '';
         }, 750);
-
         reload();
     });
 }
 
 function reload() {
-    chrome.tabs.getSelected(null, function(tab) {
-        var code = 'window.location.reload();';
-        chrome.tabs.executeScript(tab.id, {
-            code: code
+        chrome.tabs.getSelected(null, function(tab) {
+            var code = 'window.location.reload();';
+            chrome.tabs.executeScript(tab.id, {
+                code: code
+            });
         });
-    });
-}
+    }
+    // Restores select box and checkbox state using the preferences
+    // stored in chrome.storage.
 
-
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
 function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.sync.get({
@@ -38,4 +34,4 @@ function restore_options() {
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',save_options);
+document.getElementById('save').addEventListener('click', save_options);
