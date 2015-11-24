5edbcbdc63875ac99565b302d43df0a468b5609d
Robert Gabriel <rgabriel@mycit.ie>
2015-11-24 17:59:28 +0000

    Huge Fixes
    Angular  + color + font working

--- assets/js/angular/core.js
+++ assets/js/angular/core.js
@@ -1,46 +1,95 @@
-app.controller("core", function($scope) {
+var app = angular.module("Robin", []);
+app.controller("core", function ($scope) {
 
-    $scope.saved = localStorage.getItem('banndedUrls');
-    $scope.banndedUrlsList = [ {text: 'porn', done: false},{text: 'sex', done: false},{text: 'facebook', done: false},{text: 'twitters', done: false}, {text: 'rob', done: false} ];
-    $scope.themeList = [ {color: '#F44336', active : true} ];
-    $scope.banndedUrls =  (localStorage.getItem('banndedUrls')!==null) ? JSON.parse($scope.saved) : $scope.banndedUrlsList ;
-    
+    $scope.themeList = [{
+        color: '#F44336',
+        active: true
+    }];
 
     $scope.savedTheme = localStorage.getItem('theme');
-    $scope.theme = (localStorage.getItem('theme')!==null) ? JSON.parse($scope.savedTheme) : $scope.themeList ;
-    $scope.themeStyle =(localStorage.getItem('theme')!==null) ? {'background-color': $scope.theme[0][0]['color']} : console.log('no color set');
-    localStorage.setItem('banndedUrls', JSON.stringify($scope.banndedUrls));
-  
+    $scope.theme = (localStorage.getItem('theme') !== null) ? JSON.parse($scope.savedTheme) : $scope.themeList;
+    $scope.themeStyle = (localStorage.getItem('theme') !== null) ? {
+        'background-color': $scope.theme[0][0]['color']
+    } : console.log('no color set');
 
 
-    $scope.loadDefault =  function() {
-        $scope.banndedUrls =  (localStorage.getItem('banndedUrls')!==null) ? JSON.parse($scope.saved) : $scope.banndedUrlsList ;
-    }
+    var checkBox, status, code;
 
-    $scope.setColor =  function(color) {
-        alert('j');
+    $scope.init = function () {
+        // Use default value color = 'red' and likesColor = true.
+        chrome.storage.sync.get({
+            booleans: true,
+            fontSizes: "12"
+        }, function (items) {
+            document.getElementById('like').checked = items.booleans;
+            document.getElementById("number").value = items.fontSizes;
+        });
+    };
+
+
+    $scope.setColor = function (color) {
+
         $scope.removeLocalStorage('theme');
         $scope.theme = [];
-        $scope.theme.push([ {color: color, active : true} ]);
+        $scope.theme.push([{
+            color: color,
+            active: true
+        }]);
         localStorage.setItem('theme', JSON.stringify($scope.theme));
-        $scope.themeStyle={'background-color':color}
+        $scope.themeStyle = {
+            'background-color': color
+        }
     }
 
-    $scope.clearLocalStorage = function() {
-        $scope.banndedUrls = [];
-        $scope.removeLocalStorage('banndedUrls');
+    $scope.clearLocalStorage = function () {
+
         $scope.removeLocalStorage('theme');
         $scope.loadDefault();
     };
 
-    $scope.removeLocalStorage = function(key){
-
+    $scope.removeLocalStorage = function (key) {
         localStorage.removeItem(key);
     };
 
 
+
+    /**
+     * Adds Saves the Optoins 
+     */
+    $scope.openDyslexic = function () { // Saves options to chrome.storage
+        checkBox = document.getElementById('like').checked;
+        chrome.storage.sync.set({
+            booleans: checkBox
+        }, function () { // Update status to let user know options were saved.
+            reload();
+        });
+    };
+
+
+    function reload() {
+        chrome.tabs.getSelected(null, function (tab) {
+            code = 'window.location.reload();';
+            chrome.tabs.executeScript(tab.id, {
+                code: code
+            });
+        });
+    }
+
+
+    $scope.changeNumber = function () {
+        fontSize = document.getElementById('number').value;
+        chrome.storage.sync.set({
+            fontSizes: fontSize
+        }, function () { // Update status to let user know options were saved.
+            reload();
+        });
+    };
+
+
+
+
+
+
+
+
 });
-
-
-
-
--- assets/js/changeFont.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-
-    $("#size").change(function() {
-        $('.changeMe').css("font-size", $(this).val() + "px");
-    });   
-});
